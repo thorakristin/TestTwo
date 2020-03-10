01b7435427f3cc4d732565d6bb09b9825cdf2382
Þóra Kristín <thorakk19@ru.is>
2020-03-10 20:43:54 +0000

    Öll test virkar

--- sample.py
+++ sample.py
@@ -1,5 +1,7 @@
 def FizzBuzz(number):
-    if number % 3 == 0:
+    if number % 5 == 0 and number % 3 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
         return "Fizz"
     elif number % 5 == 0:
         return "Buzz"
